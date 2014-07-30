dcf61f779ba7915882909e0db4b9a38bb92c38dd
Tuhut Vlad <tuhutv@gmail.com>
2014-07-30 13:53:24 +0300

    not working

--- lib/programari.pm
+++ lib/programari.pm
@@ -20,6 +20,12 @@ use Catalyst qw/
     -Debug
     ConfigLoader
     Static::Simple
+
+    Authentication
+ 
+    Session
+    Session::Store::File
+    Session::State::Cookie
 /;
 
 extends 'Catalyst';
@@ -49,6 +55,17 @@ __PACKAGE__->config(
         INCLUDE_PATH => [
             __PACKAGE__->path_to( 'root', 'src' ),
         ],
+    },
+);
+
+# Configure SimpleDB Authentication
+__PACKAGE__->config(
+    'Plugin::Authentication' => {
+        default => {
+            class           => 'SimpleDB',
+            user_model      => 'DB::User',
+            password_type   => 'clear',
+        },
     },
 );
 
